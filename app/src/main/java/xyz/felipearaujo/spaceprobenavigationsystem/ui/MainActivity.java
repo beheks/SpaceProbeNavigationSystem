920e74338d0431ba7bb6a4c3741d43a2673c37eb
Felipe <felipe.peixoto.araujo@gmail.com>
2015-11-22 16:57:57 +0000

    Add spinner and layout changes.

--- app/src/main/java/xyz/felipearaujo/spaceprobenavigationsystem/ui/MainActivity.java
+++ app/src/main/java/xyz/felipearaujo/spaceprobenavigationsystem/ui/MainActivity.java
@@ -3,8 +3,10 @@ package xyz.felipearaujo.spaceprobenavigationsystem.ui;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,9 +27,10 @@ import xyz.felipearaujo.spaceprobenavigationsystem.interactor.SubmitData;
 public class MainActivity extends AppCompatActivity {
 
   TextView textView;
+  ProgressBar progressBar;
   Button button;
+  AlienShip mAlienShip;
 
-  @Inject AlienShip mAlienShip;
   @Inject MoveAlienShipToFinalPosition mMoveAlienShipToFinalPosition;
   @Inject SubmitData mSubmitData;
 
@@ -35,16 +38,21 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onClick(View v) {
       if(v == button) {
-
+        mAlienShip = new AlienShip(new Point(0, 0));
+        progressBar.setVisibility(View.VISIBLE);
+        button.setEnabled(false);
         //TODO Consider Extending subscriber
         mMoveAlienShipToFinalPosition.execute("test@test.com", mAlienShip).subscribe(
             new Subscriber<Point>() {
               @Override
               public void onCompleted() {
+                progressBar.setVisibility(View.GONE);
+                button.setEnabled(true);
               }
 
               @Override
               public void onError(Throwable e) {
+                textView.setText(e.getLocalizedMessage());
                 if( e instanceof IllegalStateException) {
                   Toast toast = Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT);
 
@@ -65,6 +73,8 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
 
+    mAlienShip = new AlienShip(new Point(0, 0));
+
     DaggerActivityComponent
         .builder()
         .applicationComponent(SpaceProbeNavigationSystem.getApplicationComponent())
@@ -76,7 +86,9 @@ public class MainActivity extends AppCompatActivity {
 
     textView = (TextView) findViewById(R.id.result);
     button = (Button) findViewById(R.id.button);
+    progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
+    progressBar.setVisibility(View.GONE);
     textView.setText(mAlienShip.getShipPosition().x + " " + mAlienShip.getShipPosition().y);
 
     button.setOnClickListener(mOnClickListener);
